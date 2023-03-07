60d6c435c62b65b5ae391408454564f25d359e0b
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-07 15:49:38 +0100

    added login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,4 +28,39 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user|| !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,11 @@ use Illuminate\Support\Facades\Route;
 
 // Public routes
 Route::post('/register', [AuthController::class,'register']);
+Route::post('login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
+
 
 
 // Protected routes
@@ -31,7 +33,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class,'store']);
     Route::put('/products/{id}', [ProductController::class,'update']);
     Route::delete('/products/{id}', [ProductController::class,'destroy']);
-    });
+    Route::post('/logout', [AuthController::class,'logout']);
+
+});
+
 
 
 //
