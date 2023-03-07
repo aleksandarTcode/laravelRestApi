4f5c441485d559a20719d9fd8b09384d5b712571
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-07 11:51:10 +0100

    added route protection

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+//Route::resource('products', ProductController::class);
 
+// Public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
 
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::post('/products', [ProductController::class,'store']);
-//Route::get('/products/{id}', [ProductController::class, 'show']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class,'store']);
+    Route::put('/products/{id}', [ProductController::class,'update']);
+    Route::delete('/products/{id}', [ProductController::class,'destroy']);
+    });
+
+
+//
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
