21578d9a7c40bca3a6d733e415756f0ea9f871d2
aleksandarT <aleksandar.trmcic@gmail.com>
2023-03-07 09:41:14 +0100

    added product Model and migration

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/products', function(){
+    return Product::all();
+});
+
+Route::post('/products', function(){
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
